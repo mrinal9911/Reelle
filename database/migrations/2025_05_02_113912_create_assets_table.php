a1412f8dab06118bfff17ab7e6610ff392dd1d0a
mrinal9911 <mrinalk123456@gmail.com>
2025-05-06 22:41:25 +0530

    enavle disable

--- database/migrations/2025_05_02_113912_create_assets_table.php
+++ database/migrations/2025_05_02_113912_create_assets_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
@@ -40,6 +41,7 @@ return new class extends Migration
         
             $table->timestamps();
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
